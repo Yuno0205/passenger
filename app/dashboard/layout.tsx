874141c2ebdf641e6cfcdf9221e149345e8342d6
Yuno0205 <mainhathao195@gmail.com>
2024-08-12 20:16:17 +0700

    first review

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,28 +1,16 @@
 'use client';
 import DashHeader from '@/components/DashHeader';
-import SideNav from '@/app/container/SideNav';
-import { useState } from 'react';
-import { Bars3CenterLeftIcon } from '@heroicons/react/24/outline';
+import SideNav from '../container/SideNav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [sideNavVisible, setSideNavVisible] = useState(false);
-
-  const toggleSideNav = () => {
-    setSideNavVisible(!sideNavVisible);
-  };
-
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div
-        className={`w-full flex-none md:w-64 ${
-          sideNavVisible ? 'block' : 'hidden'
-        } md:block`}
-      >
+      <div className={`hidden w-full flex-none md:block md:w-64`}>
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12 md:pt-0">
         <div className="pb-6">
-          <DashHeader onMenuClick={toggleSideNav} />
+          <DashHeader />
         </div>
         <div>{children}</div>
       </div>
--- components/DashHeader/index.tsx
+++ components/DashHeader/index.tsx
@@ -1,21 +1,33 @@
+import SideNav from '@/app/container/SideNav';
 import {
   Bars3CenterLeftIcon,
   BellIcon,
   LanguageIcon,
   MagnifyingGlassIcon,
+  MoonIcon,
+  SunIcon,
 } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export default function DashHeader({
-  onMenuClick,
-}: {
-  onMenuClick: () => void;
-}) {
+export default function DashHeader() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [sideNavVisible, setSideNavVisible] = useState(false);
+
+  const toggleSideNav = () => {
+    setSideNavVisible(!sideNavVisible);
+  };
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <div className="flex items-center justify-between py-3">
       <div className="flex gap-3">
         <Bars3CenterLeftIcon
           className="w-6 cursor-pointer md:hidden"
-          onClick={onMenuClick}
+          onClick={() => toggleSideNav()}
         />
         <form action="#" method="GET" className="hidden lg:block lg:pl-2">
           <label htmlFor="topbar-search" className="sr-only">
@@ -27,7 +39,6 @@ export default function DashHeader({
             </div>
             <input
               type="text"
-              name="email"
               id="topbar-search"
               className="focus:border-primary-500 focus:ring-primary-500 dark:focus:border-primary-500 dark:focus:ring-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-9 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 sm:text-sm"
               placeholder="Search"
@@ -35,15 +46,47 @@ export default function DashHeader({
           </div>
         </form>
       </div>
-      <div className="relative flex gap-5">
-        <LanguageIcon width={25} />
-        <BellIcon
-          // fill="#64748b"
-          color="gray-100"
-          width={25}
-          className="cursor-pointer"
-        />
-        <div className="absolute right-1 top-0 z-10 h-2 w-2 rounded-full bg-red-500"></div>
+      <div className="flex items-center gap-5">
+        <div
+          className={twMerge(
+            'flex h-10 w-10 cursor-pointer items-center justify-center rounded-xl bg-white outline outline-1 outline-gray-200 hover:bg-gray-100 ',
+            isDarkMode ? 'bg-black hover:bg-gray-600' : 'bg-white',
+          )}
+          onClick={toggleDarkMode}
+        >
+          {isDarkMode ? (
+            <MoonIcon className="w-6 cursor-pointer text-white" fill="white" />
+          ) : (
+            <SunIcon className="w-6 cursor-pointer" />
+          )}
+        </div>
+        <LanguageIcon width={25} className="w-6 cursor-pointer" />
+        <div className="relative">
+          <BellIcon
+            // fill="#64748b"
+            color="gray-100"
+            width={25}
+            className="w-6 cursor-pointer"
+          />
+          <div className="absolute right-1 top-0 z-10 h-2 w-2 rounded-full bg-red-500"></div>
+        </div>
+      </div>
+
+      {/* SideNav mobile */}
+      <div
+        className={`fixed bottom-0 left-0 right-0 top-0 z-50  bg-gray-800 bg-opacity-50 transition-opacity duration-300 ease-in-out md:block ${
+          sideNavVisible ? 'opacity-100' : 'pointer-events-none opacity-0'
+        }`}
+        onClick={() => setSideNavVisible(false)}
+      >
+        <div
+          className={`fixed left-0 top-0 h-full w-64 transform bg-white transition-transform duration-300 ease-in-out ${
+            sideNavVisible ? 'translate-x-0' : '-translate-x-full'
+          }`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <SideNav />
+        </div>
       </div>
     </div>
   );
